89a6550c72337dae46a84ad17a63666f64eb437e
David Dyer <davidrossdyer@gmail.com>
2021-10-29 14:50:29 -0400

    now creating two cards, still not using classes

--- Develop/index.js
+++ Develop/index.js
@@ -126,15 +126,13 @@ const internPrompts = [
   },
 ];
 
-const employees = [];
-
 function init() {
-  startTeam();
-  buildTeam();
+  startPage();
+  managerFirst();
 }
 
 // Function to initialize app
-function startTeam() {
+function startPage() {
   const style = `* {
     margin: 0;
     padding: 0;
@@ -231,54 +229,30 @@ function startTeam() {
     margin: 0 30px 0 10px;
   }
 `;
+  const html = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Team Manager</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" media="screen" href="./style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;500&display=swap" rel="stylesheet">
+  </head>
+  </head>
+  <body>
+    <header>
+      <h1>My Team Manager</h1>
+    </header>
+    <main>
+`;
   fs.writeFile("./dist/style.css", style, function (err) {
     if (err) {
       console.log(err);
     }
   });
-  const html = `<!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <title>Team Manager</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" type="text/css" media="screen" href="./style.css" />
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;500&display=swap" rel="stylesheet">
-      </head>
-      </head>
-      <body>
-        <header>
-          <h1>My Team Manager</h1>
-        </header>
-        <main>
-        
-          <article class="card-border">
-            <di class="card">
-              <div class="header">
-                <img id="icon" src="../Assets/icons/manager_gradient.png" />
-                <div class="nameTitle">
-                <h2>${this.name}</h2>
-                <h3>Team Manager</h3>
-                </div>
-              </div>
-              <div class="details">
-                <div class="labels">
-                  <h4>ID:</h4>
-                  <h4>Email:</h4>
-                  <h4>Office #:</h4>
-                </div>
-                <div class="this">
-                  <h4>${this.id}</h4>
-                  <h4><a href="mailto:${this.email}">${this.email}</a></h4>
-                  <h4>${this.officeNumber}</h4>
-                </div>
-              </div>
-            </div>
-          </article>
-  `;
   fs.writeFile("./dist/index.html", html, function (err) {
     if (err) {
       console.log(err);
@@ -286,11 +260,34 @@ function startTeam() {
   });
 }
 
-function buildTeam() {
+function managerFirst() {
   inquirer.prompt(managerPrompts).then((answers) => {
     console.log(answers);
-    const manager = generateEngineer(answers);
-    fs.appendFile("./dist/index.html", engineer, function (err) {
+    const manager = `<article class="card-border">
+    <di class="card">
+      <div class="header">
+        <img id="icon" src="../../Assets/icons/manager_gradient.png" />
+        <div class="nameTitle">
+        <h2>${answers.name}</h2>
+        <h3>Team Manager</h3>
+        </div>
+      </div>
+      <div class="details">
+        <div class="labels">
+          <h4>ID:</h4>
+          <h4>Email:</h4>
+          <h4>Office #:</h4>
+        </div>
+        <div class="this">
+          <h4>${answers.id}</h4>
+          <h4><a href="mailto:${answers.email}">${answers.email}</a></h4>
+          <h4>${answers.officeNumber}</h4>
+        </div>
+      </div>
+    </div>
+  </article>
+`;
+    fs.appendFile("./dist/index.html", manager, function (err) {
       if (err) {
         console.log(err);
       }
@@ -312,40 +309,156 @@ function buildTeam() {
   });
 }
 
-function printTeam() {
-  return ` 
-  <article class="card-border">
-            <di class="card">
-              <div class="header">
-                <img id="icon" src="../Assets/icons/manager_gradient.png" />
-                <div class="nameTitle">
-                <h2>${this.name}</h2>
-                <h3>${this.role}</h3>
-                </div>
-              </div>
-              <div class="details">
-                <div class="labels">
-                  <h4>ID:</h4>
-                  <h4>Email:</h4>
-                  <h4>${this.role}</h4>
-                </div>
-                <div class="this">
-                  <h4>${this.id}</h4>
-                  <h4><a href="mailto:${this.email}">${this.email}</a></h4>
-                  <h4>${this.officeNumber}</h4>
-                  <h4><a href="https://www.github.com/${this.github}" target="_blank">${this.github}</a></h4>
-                  <h4><a href="${this.portfolio}" target="_blank"></a></h4>
-                  <h4>${this.school}</h4>
-                </div>
-              </div>
-            </div>
-          </article>
-  `;
+function inquirerEngineer() {
+  inquirer.prompt(engineerPrompts).then((answers) => {
+    console.log(answers);
+    const engineer = `<article class="card-border">
+    <di class="card">
+      <div class="header">
+        <img id="icon" src="../../Assets/icons/engineer_gradient.png" />
+        <div class="nameTitle">
+        <h2>${answers.name}</h2>
+        <h3>Engineer</h3>
+        </div>
+      </div>
+      <div class="details">
+        <div class="labels">
+          <h4>ID:</h4>
+          <h4>Email:</h4>
+          <h4>GitHub:</h4>
+        </div>
+        <div class="answers">
+          <h4>${answers.id}</h4>
+          <h4><a href="mailto:${answers.email}">${answers.email}</a></h4>
+          <h4><a href="https://www.github.com/${answers.github}" target="_blank">${answers.github}</a></h4>
+        </div>
+      </div>
+    </div>
+  </article>
+`;
+    fs.appendFile("./dist/index.html", engineer, function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+    switch (answers.employee_type) {
+      case "Engineer":
+        inquirerEngineer();
+        break;
+      case "Designer":
+        inquirerDesigner();
+        break;
+      case "Intern":
+        inquirerIntern();
+        break;
+      default:
+        closeHTML();
+        break;
+    }
+  });
+}
+
+function inquirerDesigner() {
+  inquirer.prompt(designerPrompts).then((answers) => {
+    console.log(answers);
+    const designer = `<article class="card-border">
+    <di class="card">
+      <div class="header">
+        <img id="icon" src="../../Assets/icons/designer_gradient.png" />
+        <div class="nameTitle">
+        <h2>${answers.name}</h2>
+        <h3>Designer</h3>
+        </div>
+      </div>
+      <div class="details">
+        <div class="labels">
+          <h4>ID:</h4>
+          <h4>Email:</h4>
+          <h4>Portfolio:</h4>
+        </div>
+        <div class="answers">
+          <h4>${answers.id}</h4>
+          <h4><a href="mailto:${answers.email}">${answers.email}</a></h4>
+          <h4><a href="${answers.portfolio}" target="_blank"></a></h4>
+        </div>
+      </div>
+    </div>
+  </article>
+`;
+    fs.appendFile("./dist/index.html", designer, function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+    switch (answers.employee_type) {
+      case "Engineer":
+        inquirerEngineer();
+        break;
+      case "Designer":
+        inquirerDesigner();
+        break;
+      case "Intern":
+        inquirerIntern();
+        break;
+      default:
+        closeHTML();
+        break;
+    }
+  });
+}
+
+function inquirerIntern() {
+  inquirer.prompt(internPrompts).then((answers) => {
+    console.log(answers);
+    const intern = `<article class="card-border">
+    <di class="card">
+      <div class="header">
+        <img id="icon" src="../../Assets/icons/intern.png" />
+        <div class="nameTitle">
+        <h2>${answers.name}</h2>
+        <h3>Intern</h3>
+        </div>
+      </div>
+      <div class="details">
+        <div class="labels">
+          <h4>ID:</h4>
+          <h4>Email:</h4>
+          <h4>School:</h4>
+        </div>
+        <div class="answers">
+          <h4>${answers.id}</h4>
+          <h4><a href="mailto:${answers.email}">${answers.email}</a></h4>
+          <h4>${answers.school}</h4>;
+        </div>
+      </div>
+    </div>
+  </article>
+`;
+    fs.appendFile("./dist/index.html", intern, function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+    switch (answers.employee_type) {
+      case "Engineer":
+        inquirerEngineer();
+        break;
+      case "Designer":
+        inquirerDesigner();
+        break;
+      case "Intern":
+        inquirerIntern();
+        break;
+      default:
+        closeHTML();
+        break;
+    }
+  });
 }
 
 function closeHTML() {
   const closeHTML = `</main></body></html>`;
-  fs.appendFile("index.html", closeHTML, function (err) {
+  fs.appendFile("./dist/index.html", closeHTML, function (err) {
     if (err) {
       console.log(err);
     }
